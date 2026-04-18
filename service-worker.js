7e070e05def97458408daf936f4c5a7f2cdc3e6a
Praveen <praveen01kashyap@gmail.com>
2026-04-19 02:17:33 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'rate-it-v1';
+
+// Install and Cache Files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+// Serve from Cache if Offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
